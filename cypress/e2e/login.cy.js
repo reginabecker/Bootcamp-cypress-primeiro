bc3460940b26907c07bb4b55c0ce7fb2f38fa7af
Regina Becker <thinaregina@gmail.com>
2023-08-23 12:25:12 -0300

    Run pipeline

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,7 +6,7 @@ describe('Login', () => {
         cy.visit('/login')
         cy.get('#user').type('thinaregina@gmail.com')
         //cy.get('#user').type('thinaregina@gmail.com{enter}')
-        cy.get('#password').type('$thina$2023')
+        cy.get('#passwordW').type('$thina$2023')
         cy.get('#btnLogin').click()
         cy.url().should('include', 'my-account')
     })
